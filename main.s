c247c1873b6c46637f2eee8cc6c18a977ec6dde7
LJY <n26132097@gs.ncku.edu.tw>
2024-10-18 16:47:05 +0800

    完成 Task 3.1 (Dot Product)

--- main.s
+++ main.s
@@ -1,6 +1,13 @@
 .data
 array: .word 7,-12,-1421,-22,-14,20,9,0,-14
 array_length: .word 9
+array1:.word 1,2,3,4,5,6,7,8,9
+array2:.word 6,1,6,1,6,1,6,1,6
+array_stride1:.word 2
+array_stride2:.word 2
+array_length1:.word 9
+array_length2:.word 9
+calculation_element:.word 5
 .text
 
 main:
@@ -39,6 +46,61 @@ main:
     addi sp, sp, 12
     #-------------------------#
 
+    # load array information
+    la a0, array1
+    la a1, array2
+    la a2, calculation_element
+    la a3, array_stride1
+    la a4, array_stride2
+    # check element will not cause an out-of-bound
+    la t0, array_length1
+    la t1, array_length2
+    lw t0, 0(t0)
+    lw t1, 0(t1)
+    lw t2, 0(a3)
+    lw t3, 0(a4)
+    # calculate ceil(array_length1/array_stride1)
+    div t4, t0, t2
+    rem t6, t0, t2
+    beq t6, zero, check1_end
+    addi t4, t4, 1
+check1_end:
+    # calculate ceil(array_length1/array_stride1)
+    div t5, t1, t3
+    rem t6, t1, t3
+    beq t6, zero, check2_end
+    addi t5, t5, 1
+check2_end:
+    # find min(t4, t5)
+    blt t4, t5, check3_end
+    addi t4, t5, 0
+check3_end:
+    # ensure t6 less than t4 value
+    lw t6, 0(a2)
+    blt t6, t4, Exit
+
+    #-----Call DotProduct-----#
+    # store argument
+    addi sp, sp, -24
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+    sw a1, 8(sp)
+    sw a2, 12(sp)
+    sw a3, 16(sp)
+    sw a4, 20(sp)
+    # jump to DotProdunct
+    jal DotProduct # return
+    # restore argument
+    lw ra, 0(sp)
+    lw a0, 4(sp)
+    lw a1, 8(sp)
+    lw a2, 12(sp)
+    lw a3, 16(sp)
+    lw a4, 20(sp)
+    addi sp, sp, 24
+    #-------------------------#
+
+Exit:
     # Exit program
     li a0,10
     ecall
@@ -47,7 +109,9 @@ main:
 #假設call ReLU 已經將 la a0,array、la a1,array_length
 ReLU:
     # check array_length ≥ 1
-    blt a1, zero, ReLU_error
+    li t0, 1
+    lw t1, 0(a1)
+    blt t1, t0, ReLU_error
 
     # load array_length count
     lw t1, 0(a1)
@@ -74,11 +138,13 @@ ReLU_error:
 #-----------------------------------------------------#
 
 #-------------------ArgMax function-------------------#
-#假設call ReLU 已經將 la a0,array、la a1,array_length
+#假設call ArgMax 已經將 la a0,array、la a1,array_length
 ArgMax:
     # check array_length ≥ 1
-    blt a1, zero, ReLU_error
-    
+    li t0, 1
+    lw t1, 0(a1)
+    blt t1, t0, ReLU_error
+
     # load array_length count
     lw t1, 0(a1)
     # load number from memory
@@ -100,5 +166,60 @@ ArgMax_done:
     jr ra
 ArgMax_error:
     li a0, 36
+    jr ra
+#-----------------------------------------------------#
+
+#-----------------DotProduct function-----------------#
+#假設call DotProduct 已經將 la a0,array1、la a1,array2、la a2,calculation_element、la a3,array_stride1、la a4 array_stride2
+DotProduct:
+    # store temp reg.
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    addi s0, zero, 0
+    
+
+    # check calculation_element ≥ 1
+    li t0, 1
+    lw t1, 0(a2)
+    blt t1, t0, DotProduct_error_element
+    # check array_stride ≥ 1
+    lw t1 0(a3)
+    blt a3, t0, DotProduct_error_stride
+    lw t1 0(a4)
+    blt a4, t0, DotProduct_error_stride
+
+    # load calculation element count
+    lw t2, 0(a2)
+    # load array_stride
+    lw t3, 0(a3)
+    lw t4, 0(a4)
+    # change to stride*4
+    slli t3, t3, 2
+    slli t4, t4, 2
+DotProduct_loop:
+    # load number from memory
+    lw t0, 0(a0)
+    lw t1, 0(a1)
+    mul s1, t0, t1
+    add s0, s0, s1
+DotProduct_done:
+    addi t2, t2, -1
+    add a0, a0, t3
+    add a1, a1, t4
+    bne zero, t2, DotProduct_loop
+    addi a0, s0, 0
+
+    # restore temp reg.
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    addi sp, sp, 8
+
+    jr ra
+DotProduct_error_element:
+    li a0, 36
+    jr ra
+DotProduct_error_stride:
+    li a0, 37
     jr ra
 #-----------------------------------------------------#
